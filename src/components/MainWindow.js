42c4a6f674c6dba9c4e28b698e9cd7b3ee984f3d
Ingrid <ingrid.meccas@gmail.com>
2022-05-03 15:18:58 +0100

    Style added to main window and queue + CSS updated

--- src/components/MainWindow.js
+++ src/components/MainWindow.js
@@ -1,6 +1,11 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import { IoIosAddCircle, IoIosPlayCircle, IoIosStarOutline } from "react-icons/io";
+import { Container, Card, CardGroup, CardImg } from "react-bootstrap";
+import {
+  IoIosAddCircle,
+  IoIosPlayCircle,
+  IoIosStarOutline,
+} from "react-icons/io";
+import path from "path";
 import "../App.css";
 
 export const MainWindow = ({ music, addToQueue, playSong, addToFavorites }) => {
@@ -10,30 +15,41 @@ export const MainWindow = ({ music, addToQueue, playSong, addToFavorites }) => {
     return songName.slice(-4);
   };
   return (
-    <Container>
+    <Container className="border bccard col-9 pt-5 pb-5 px-4">
       {music.map((song) => (
         
-          <div class="" key={song.id}>               
+          <div class="card2 bgimg mt-4 col-3 p-2 shadow-md rounded text-center"          
+            key={song.id}>
+               <img class="bird" src="https://cdn-icons-png.flaticon.com/512/3362/3362384.png" width= "50px" height="50px"/>
             {song.songPath
               .trim()
               .split("\\")
               .slice(-1)
               .toString()
               .slice(12, -4).slice(0, 20).concat('...') }
-            <div class="">
-              <button onClick={(e) => {
+            <div class="p-1">
+              <button
+                type="button"
+                className="btn btn-light p-3 rounded-10 border"
+                onClick={(e) => {
                   playSong(song.id);
                 }}
               >
                 <IoIosPlayCircle />
               </button>
-              <button onClick={(e) => {
+              <button
+                type="button"
+                className="btn btn-light p-3 rounded-10 border"
+                onClick={(e) => {
                   addToQueue(song.id);
                 }}
               >
                 <IoIosAddCircle />
               </button>
-              <button onClick={(e) => {
+              <button
+                type="button"
+                className="btn btn-light p-3 rounded-10 border"
+                onClick={(e) => {
                   addToFavorites(song.id);
                 }}
               >
--- src/components/Queue.js
+++ src/components/Queue.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Card, Container } from 'react-bootstrap';
 import { IoIosCloseCircle } from 'react-icons/io';
-
 function Queue({ queue, deleteFromQueue }) {
    return (        
-      <Container>
+      <Container className="border bcqueue col-3 float-right pt-5 pb-5">
          {queue.map((song) => (
-            <Card 
+            <Card className= "card p-1 shadow-sm bg-light rounded text-center"
             key={Math.random()}>
                {song.songPath
                   .trim()
@@ -14,7 +13,9 @@ function Queue({ queue, deleteFromQueue }) {
                   .slice(-1)
                   .toString()
                   .slice(12, -4)}
-               <button onClick={() => deleteFromQueue(song.id)}>
+               <button button
+                type="button"
+                className="btn p-2 btn-outline-danger" onClick={() => deleteFromQueue(song.id)}>
                   <IoIosCloseCircle />
                </button>
             </Card>
