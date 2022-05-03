890b56a72b4377a459ea1197989c624758c5e49e
Robert Szlufik <robert.szlufik123@gmail.com>
2022-05-03 13:15:08 +0100

    testing fixed

--- test/main.test.js
+++ test/main.test.js
@@ -2,33 +2,33 @@ const { Application } = require('spectron');
 const assert = require('assert');
 const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
 const path = require('path');
-const { app } = require('electron');
+// const { app } = require('electron');
 
 const sleep = (time) => new Promise((r) => setTimeout(r, time));
-// let app;
+let app;
 
 describe('Application launch', function () {
    this.timeout(30000);
 
    beforeEach(async function () {
-      this.app = new Application({
+      app = new Application({
          path: electronPath,
          args: [path.join(__dirname, '..')],
       });
-      return await this.app.start();
+      return await app.start();
    });
 
    afterEach(async function () {
-      if (this.app && this.app.isRunning()) {
-         return await this.app.stop();
+      if (app && app.isRunning()) {
+         return await app.stop();
       }
    });
 
    it('Shows an initial window', async () => {
-      // const a = await this.app;
+      // const a = await app;
       // assert.equal(a, undefined);
-      await this.app.client.waitUntilWindowLoaded();
-      const count = await this.app.client.getWindowCount();
+      await app.client.waitUntilWindowLoaded();
+      const count = await app.client.getWindowCount();
       assert.equal(count, 1);
    });
 
