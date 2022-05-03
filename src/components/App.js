5a509c1507a96a2df47fe766caba9b9e73b6f68d
Gabriel <gabriel.pa16@gmail.com>
2022-05-03 13:05:37 +0100

    Fetching music from main process

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,20 @@ import MainWindow from './MainWindow';
 import Player from './player';
 import NavBar from './NavBar';
 import Queue from './Queue';
+import { ipcRenderer } from 'electron';
+
+const App = () => {
+
+   const [music, setMusic] = useState([]);
+
+   useEffect(() => {
+      ipcRenderer.send('get:music');
+      ipcRenderer.on('send:music', (e, songs) => {
+         // console.log(JSON.parse(songs));
+         setMusic(JSON.parse(songs));
+      });
+   }, 
+   []);
 
 const App = () => {
    return (
