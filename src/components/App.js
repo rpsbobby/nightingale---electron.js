bd818b75c639ba354645db714b6a909052a7931b
Gabriel <gabriel.pa16@gmail.com>
2022-05-03 14:17:31 +0100

    making main methods

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,39 @@ import { ipcRenderer } from 'electron';
 const App = () => {
    const [music, setMusic] = useState([]);
    const [queue, setQueue] = useState([]);
+   const [nextSong, setNextSong] = useState('');
+
+   const [play, setPlay] = useState();
+
+   const addToQueue = (id) => {
+      console.log(nextSong);
+      setQueue([...queue, music[id]]);
+      if (nextSong === '') {
+         setNextSong(id);
+      }
+   };
+
+   const playNextSongFromQueue = () => {
+      console.log(nextSong);
+      console.log(queue[0].id);
+      if (nextSong === queue[0].id) {
+         setNextSong('');
+      }
+      setNextSong(queue[0].id);
+      setQueue(queue.slice(1));
+      console.log(nextSong);
+   };
 
    const deleteFromQueue = (id) => {
       const temp = queue.filter((song) => song.id !== id);
       setQueue(temp);
    };
+
+   const playSong = (id) => {
+      setNextSong(id);
+   };
+
+   useEffect(() => {}, [nextSong]);
 
    useEffect(() => {
       ipcRenderer.send('get:music');
@@ -25,9 +53,17 @@ const App = () => {
    return (
       <div>
          <NavBar />
-         <MainWindow />
+         <MainWindow 
+         music={music}
+               addToQueue={addToQueue}
+               playSong={playSong}
+         />
          <Queue queue={queue} deleteFromQueue={deleteFromQueue} />
-         <Player />
+         <Player 
+           music={music}
+           id={nextSong}
+           playNextSongFromQueue={playNextSongFromQueue}
+           />
       </div>
    );
 };
