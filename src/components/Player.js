91a5e2289b3e57c1c04114b3eb96f9b73b086943
Gabriel <gabriel.pa16@gmail.com>
2022-05-03 13:09:58 +0100

    implement functionality in player.js

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import { Button, Container } from 'react-bootstrap';
 
-function Player() {
+function Player({ music, id, playNextSongFromQueue }) {
     
-    return (
-        <Container>player</Container>
-    );
-}
+    let songPath = '';
+
+    if (id) {
+       songPath = music[id].songPath.toString();
+    }
+ 
+    function ended(e) {
+       if (e) {
+          playNextSongFromQueue();
+       }
+    }
+ 
+    if (songPath !== '') {
+       return (
+          <Container className="d-flex justify-content-center bcplayer col-12 fixed-bottom">
+             {/* <audio src={music} controls autoPlay> */}
+             <audio
+                onEnded={(e) => ended(e)}
+                id="audio"
+                style={{ width: '70%' }}
+                src={songPath}
+                controls
+                autoPlay
+             >
+                <source type="audio/mpeg"></source>
+                <source src={songPath} type="audio/mpeg"></source>
+                {/* <source src={songToPlay} type="audio/mpeg"></source> */}
+             </audio>
+          </Container>
+       );
+    } else {
+       return (
+          <Container className="d-flex justify-content-center bcplayer col-12 fixed-bottom">
+             {/* <audio src={music} controls autoPlay> */}
+             <audio
+                onEnded={(e) => ended(e)}
+                id="audio"
+                style={{ width: '70%' }}
+                controls
+                autoPlay
+             >
+                <source type="audio/mpeg"></source>
+                {/* <source src={songToPlay} type="audio/mpeg"></source> */}
+             </audio>
+          </Container>
+       );
+    }
+ }
  
  
  export default Player;
