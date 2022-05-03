97b51eaef5f0b7c53e8a37fdba7d4882f04f8d1f
Robert Szlufik <robert.szlufik123@gmail.com>
2022-05-03 16:06:17 +0100

    bug fix -- import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
 let root = document.createElement('div');
